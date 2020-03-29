b2e9a936dca73513c1f5b61695cdfa67b987caf5
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-03-29 23:09:21 +0500

    added public folder

---
+++ public/precache-manifest.3d44dbc5980e2a12fc233fe76a769e9a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a4e74d168d1e160c3e95f6ae1e6a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8046d744b923bbd15927",
+    "url": "/static/css/2.33e0e918.chunk.css"
+  },
+  {
+    "revision": "8362c7572306c923b260",
+    "url": "/static/css/main.37cffc5e.chunk.css"
+  },
+  {
+    "revision": "8046d744b923bbd15927",
+    "url": "/static/js/2.409b9564.chunk.js"
+  },
+  {
+    "revision": "8362c7572306c923b260",
+    "url": "/static/js/main.dcae1514.chunk.js"
+  },
+  {
+    "revision": "adecb6c846a07183098f",
+    "url": "/static/js/runtime~main.8e789d1b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
